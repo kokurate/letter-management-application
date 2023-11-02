5fb59a861c0a001baebab20de84a3a9b4039f033
kokurate <fredriksanger@gmail.com>
2023-11-02 23:54:00 +0800

    Implement initial design for the Pegawai page

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -22,7 +22,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <a class="navbar-brand" href="#page-top">DPRKP</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto my-2 my-lg-0">
@@ -50,12 +50,15 @@
             <div class="container px-4 px-lg-5 h-100">
                 <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-8 align-self-end">
-                        <h1 class="text-white font-weight-bold">Your Favorite Place for Free Bootstrap Themes</h1>
+                        <h2 class="text-white font-weight-bold" style="font-size: 35px;">
+                            <strong>Dinas Perumahan Rakyat Dan Kawasan Pemukiman Kabupaten Minahasa</strong>
+                        </h2>
                         <hr class="divider" />
                     </div>
                     <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-                        <a class="btn btn-primary btn-xl" href="#upload">Find Out More</a>
+                        <p class="text-white-75 mb-5" style="font-size: 25px">Selamat Datang Pegawai</p>
+                        <p class="text-white-75 mb-5" style="font-size: 25px;margin-top:-40px" >{{ auth()->user()->name }}</p>
+                        <a class="btn btn-primary btn-xl" href="#upload">Upload Surat</a>
                     </div>
                 </div>
             </div>
@@ -67,71 +70,72 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 col-xl-6 text-center">
-                        <h2 class="mt-0">Let's Get In Touch!</h2>
+                        <h1 class="mt-0">Upload Surat</h1>
                         <hr class="divider" />
-                        <p class="text-muted mb-5">Ready to start your next project with us? Send us a messages and we will get back to you as soon as possible!</p>
+                        {{-- <p class="text-muted mb-5">Ready to start your next project with us? Send us a messages and we will get back to you as soon as possible!</p> --}}
                     </div>
                 </div>
                 <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
+                    <div class="col-lg-12">
+                        <form action="insertpegawai" method="POST" enctype="multipart/form-data" id="contactForm" >
+                            @csrf   
+                            <div class="mb-3">
+                                <label for="tipe_surat" class="form-label">Tipe Surat</label>
+                                <select name="tipe_surat" id="tipe_surat" class="form-select" >
+                                    <option value="">Pilih Tipe Surat</option>
+                                    <option value="Surat Masuk">Surat Masuk</option>
+                                    <option value="Surat Keluar">Surat Keluar</option>
+                                </select>
+                            </div>
+                    </div>
                     <div class="col-lg-6">
-                        <!-- * * * * * * * * * * * * * * *-->
-                        <!-- * * SB Forms Contact Form * *-->
-                        <!-- * * * * * * * * * * * * * * *-->
-                        <!-- This form is pre-integrated with SB Forms.-->
-                        <!-- To make this form functional, sign up at-->
-                        <!-- https://startbootstrap.com/solution/contact-forms-->
-                        <!-- to get an API token!-->
-                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                            <!-- Name input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                                <label for="name">Full name</label>
-                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                            </div>
-                            <!-- Email address input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
-                                <label for="email">Email address</label>
-                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                            </div>
-                            <!-- Phone number input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
-                                <label for="phone">Phone number</label>
-                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                            </div>
-                            <!-- Message input-->
-                            <div class="form-floating mb-3">
-                                <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
-                                <label for="message">Message</label>
-                                <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                            </div>
-                            <!-- Submit success message-->
-                            <!---->
-                            <!-- This is what your users will see when the form-->
-                            <!-- has successfully submitted-->
-                            <div class="d-none" id="submitSuccessMessage">
-                                <div class="text-center mb-3">
-                                    <div class="fw-bolder">Form submission successful!</div>
-                                    To activate this form, sign up at
-                                    <br />
-                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                <div class="mb-3">
+                                    <label for="no_surat" class="form-label">No Surat</label>
+                                    <input type="text" class="form-control" id="no_surat" name="no_surat">
                                 </div>
+
+                                <div class="mb-3">
+                                    <label for="perihal_pegawai" class="form-label">Perihal</label>
+                                    <input type="text" class="form-control" id="perihal_pegawai" name="perihal_pegawai">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="tanggal" class="form-label">Tanggal Pelaksanaan</label>
+                                    <input type="date" id="tanggal" 
+                                            class="form-control" name="tanggal" value="" required>
+                                    
+                                </div>
+                                
+                            
+                    </div>
+                    <div class="col-lg-6">
+
+                            <div class="mb-3">
+                                <label for="alamat_pengirim" class="form-label">Alamat Pengirim</label>
+                                <input type="text" class="form-control" id="alamat_pengirim" name="alamat_pengirim">
                             </div>
-                            <!-- Submit error message-->
-                            <!---->
-                            <!-- This is what your users will see when there is-->
-                            <!-- an error submitting the form-->
-                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
-                            <!-- Submit Button-->
-                            <div class="d-grid"><button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Submit</button></div>
+
+                            <div class="mb-3">
+                                <label for="alamat_tujuan" class="form-label">Alamat Tujuan</label>
+                                <input type="text" class="form-control" id="alamat_tujuan" name="alamat_tujuan">
+                            </div>
+
+                            
+                            <div class="mb-3">
+                                <label for="file" class="form-label">File Surat</label>
+                                <input class="form-control" type="file" id="file">    
+                            </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="d-grid"><button class="btn btn-primary btn-xl mt-5" id="submitButton" type="submit">Submit</button></div>
+
                         </form>
                     </div>
                 </div>
             </div>
 
-            <br><br>
+            <!-- Table Daftar Surat
+                <br><br>
             <div class="container px-4 px-lg-5 mt-5" id="history">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 col-xl-6 text-center">
@@ -141,16 +145,16 @@
                     </div>
                 </div>
             </div>
-
+            -->
 
         </section>
 
    
 
         <!-- Footer-->
-        <footer class="bg-light py-5">
+        {{-- <footer class="bg-light py-5">
             <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2023 - Company Name</div></div>
-        </footer>
+        </footer> --}}
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- SimpleLightbox plugin JS-->
